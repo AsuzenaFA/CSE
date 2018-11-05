d7fba8326443bef5b1865ab3ec0c9a7a3ac33288
AsuzenaFA <42383455+AsuzenaFA@users.noreply.github.com>
2018-11-05 08:43:53 -0800

    guess game

--- notes/Asuzena - Guess Game.py
+++ notes/Asuzena - Guess Game.py
@@ -1,14 +1,19 @@
 import random
-a = (random.randint(1, 10))
-guess = int(input("Guess a number form 1 to 10 "))
-while a != "guess":
-    for i in range(5):
-        if guess < a:
-            print("too low")
-            guess = int(input("Guess a noumber for 1 to 10 "))
-        elif guess > a:
-            print("too high")
-            guess = int(input("guess a noumber form 1 to 10 "))
-        elif a == guess:
-            for i in {1}:
-                print("correct")
+number = (random.randint(1, 10))
+guess = + 1
+guess_taken = guess
+print("I am thinking of a number between 1 and 10")
+print(input(" take a guess "))
+while guess_taken < 5:
+    if guess < number:
+        print("too low, guess higher")
+    if guess > number:
+        print("too high, guess lower")
+
+
+
+
+
+
+
+
