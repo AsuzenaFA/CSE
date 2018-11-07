123c9ab6a5de1f35cc0e2664148004c610141930
AsuzenaFA <42383455+AsuzenaFA@users.noreply.github.com>
2018-11-07 08:44:26 -0800

    guess game 11 7

--- notes/Asuzena - Guess Game.py
+++ notes/Asuzena - Guess Game.py
@@ -1,19 +1,21 @@
 import random
+guess_left = 5
 number = (random.randint(1, 10))
-guess = + 1
-guess_taken = guess
-print("I am thinking of a number between 1 and 10")
-print(input(" take a guess "))
-while guess_taken < 5:
-    if guess < number:
-        print("too low, guess higher")
-    if guess > number:
-        print("too high, guess lower")
+win = False
+guess = int(input("Guess a number between 1 and 10"))
+while guess_left > 0 and not win:
+    print("i am thinking of a number between 1 and 10")
+    print("take a guess ")
 
-
-
-
-
-
-
-
+    guess_left -= 1
+    if guess == number:
+     print("correct you guess it")
+     win = False
+    else:
+        if number > guess:
+            print("to low")
+        elif number < guess:
+            print("to high")
+     if guess_left == 0:
+         print("ran out of guesses")
+         win = True
