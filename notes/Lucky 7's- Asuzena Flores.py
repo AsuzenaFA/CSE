c244e0aec363db821c4cc879f3129b868c0776c8
AsuzenaFA <42383455+AsuzenaFA@users.noreply.github.com>
2018-11-13 08:41:55 -0800

    lucky 7's 11/13

--- notes/Lucky 7's- Asuzena Flores.py
+++ notes/Lucky 7's- Asuzena Flores.py
@@ -2,17 +2,16 @@ import random
 dice1 = random.randint(1, 6)
 dice2 = random.randint(1, 6)
 win = False
-bank = 50
-roll = int
-rolling = dice1 and dice2
-int(input("Type 'roll' to throw the dice:  "))
-while rolling:
+bank = 15
+while bank > 0:
     if dice1 + dice2 == 7:
         win = True
-        bank = bank + 10
+        bank = bank + 4
     elif dice1 + dice2 < 7:
         win = False
-        bank = bank - 10
+        bank = bank - 1
     elif dice1 + dice2 > 7:
         win = False
-        bank = bank - 10
+        bank = bank - 1
+        print(dice1 + dice2)
+print("Your current amount is %s." % bank)
