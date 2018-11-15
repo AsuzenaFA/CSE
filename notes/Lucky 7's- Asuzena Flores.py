d3de64ac10a0247e23ceaf6ec2ff59a11d2a4a9a
AsuzenaFA <42383455+AsuzenaFA@users.noreply.github.com>
2018-11-15 08:24:03 -0800

    finished lucky 7's

--- notes/Lucky 7's- Asuzena Flores.py
+++ notes/Lucky 7's- Asuzena Flores.py
@@ -1,20 +1,22 @@
 import random
-dice1 = random.randint(1, 6)
-dice2 = random.randint(1, 6)
-rounds = 0
-win = False
+roll_again = "yes"
 bank = 15
+rounds = 0
 while bank > 0:
+    print("Rolling Dice...")
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
+    print("You got")
+    print(dice1)
+    print(dice2)
     rounds += 1
-    if dice1 + dice2 == 7:
-        win = True
-        bank = bank + 4
-    elif dice1 + dice2 < 7:
-        win = False
-        bank = bank - 1
-    elif dice1 + dice2 > 7:
-        win = False
-        bank = bank - 1
-        print(dice1 + dice2)
-print("Your current amount is %s." % bank)
-
+    bank -= 1
+    myrole = (dice1 + dice2)
+    if myrole == 7:
+        bank += 5
+        print("You got 7 you got 4 dollars." )
+    elif myrole != 0:
+        print("You didn't get a 7, roll again? ")
+    elif bank != 0:
+        print("You don't have enough money")
+print("You lose, you played for %d rounds" % rounds)
