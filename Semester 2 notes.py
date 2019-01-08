89240434eeb0a5e5ecb2b74eac74a7b7aad6aaf1
AsuzenaFA <42383455+AsuzenaFA@users.noreply.github.com>
2019-01-08 08:44:32 -0800

    s2n

---
+++ Semester 2 notes.py
@@ -0,0 +1,17 @@
+'''
+# REVIEW
+print("Hello World")
+# the
+car = 5
+driving = True
+print("I have %s cars" % car)
+print("I have " + str(car) + " cars")
+
+age = input("How old are you? 4")
+print(age + "! your that old.")
+'''
+colors = ["brown", "gray", "pink", "blue", "purple"]
+colors.append("green")
+print(colors)
+colors.pop(0)
+print(colors)
