102c6025704035772ce75510c191a6d514e6e802
AsuzenaFA <42383455+AsuzenaFA@users.noreply.github.com>
2019-01-25 08:44:25 -0800

    hm 1/25/19

--- AsuzenaFA-CSE Hangman.py
+++ AsuzenaFA-CSE Hangman.py
@@ -1,34 +1,45 @@
-
-import string
 import random
-word_bank = ["pink dolphin", "Spongebob", "Aurthor", "Princess Peach", "Mario", "Bambi", "Stitch", "Pepa Pig",
+word_bank = ["pink dolphin", "Spongebob", "Arthur", "Princess Peach", "Mario", "Bambi", "Stitch", "Pepa Pig",
              "Bowser", "potato head", ]
 
-list1 = (list(string.ascii_letters))
 guessed_letters = []
-string = (random.choice(word_bank))
-"-".join(string)
-word = "-" * len(string)
-print(word)
+string = random.choice(word_bank)
+list_of_letters = list(string.lower())
+word = list("-" * len(string))
+print("".join(word))
 guess = input("Take a guess: ")
 
 win = False
 turn = 8
-guessed = 8
-current_index = word.index(word)
 
-while turn > 0 and not word:
-    guessed -= 1
-    for character in range(len(word)):
-        if guess.lower() == word:
-            guess.append(guessed_letters)
-            string.pop(current_index)
-            string.insert(current_index, guess)
-            print(word)
 
-        elif guess.upper() == word:
-            string.pop(current_index)
-            string.insert(current_index, guess)
-            print(word)
-            input("Take another guess")
-        elif guess != word:
+while turn > 0 and not win:
+    current_guess = input("Take a guess: ")
+    turn -= 1
+    guessed_letters.append(guess)
+    if current_guess.lower() in list_of_letters:
+        print("CORRECT!")
+        for pos in range(len(word)):
+            if list_of_letters[pos].lower() == current_guess.lower():
+                word.pop(pos)
+                word.insert(pos, list_of_letters[pos])
+                print("".join(word))
+    elif current_guess.upper() in list_of_letters:
+        print("CORRECT!")
+        for pos in range(len(word)):
+            if list_of_letters[pos].upper() == current_guess.upper():
+                word.pop(pos)
+                word.insert(pos, list_of_letters)
+                print("".join(word))
+    if current_guess in guessed_letters:
+        print("You already guessed that")
+    elif current_guess not in word:
+        print("WRONG")
+
+    if turn < 0:
+        win = False
+    elif turn < 0:
+        print("You lose")
+    elif current_guess is word:
+            win = True
+            print("You guessed it, the word was %s" % word)
