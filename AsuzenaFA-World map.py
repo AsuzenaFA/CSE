c7eb070445d8e851dff90f954a9d6e5db4a8d2c3
AsuzenaFA <42383455+AsuzenaFA@users.noreply.github.com>
2019-02-05 08:44:31 -0800

    map

--- AsuzenaFA-World map.py
+++ AsuzenaFA-World map.py
@@ -6,13 +6,14 @@ world_map = {
         'PATHS': {
             'EAST': "BACKYARD",
             'WEST': "LAWN"
+            # DOWN : CLOSET
         }
     },
     'BACKYARD': {
         'NAME': "Your backyard",
         'DESCRIPTION': "You look around and there is an axe in a log and there is"
                        " forest to the west, north, and a water fountain to the south.",
-        'PATHS' : {
+        'PATHS': {
             'EAST': "SPAWN",
             'WEST': "WEST_FOREST",
             'NORTH': "NORTH_FOREST",
@@ -22,6 +23,35 @@ world_map = {
     },
     'FRONTYARD': {
         'NAME': "Your lawn",
-        'DESCRIPTION': "You look around and see "
+        'DESCRIPTION': "You look around and see the water fountain to the south, forest to the north,and a big wooden"
+                       "gate to the east.",
+        'PATHS': {
+            'EAST': "GATES",
+            'WEST': "SPAWN",
+            'NORTH': "NORTH_FOREST",
+            'SOUTH': "WATER_FOUNTAIN"
+        }
+    },
+    'GATES': {
+        'NAME': "Wooden Gates",
+        'DESCRIPTION': "You enter and you are at the front fo the village, "
+                       "you see a doctor with nurses and a big oak tree behind them to the north, "
+                       "a merchant to the south, and a castle far towards the east",
+        'PATHS': {
+            'EAST': 'BACK_VILLAGE',
+            'NORTH': 'HEALING_TREE',
+            'SOUTH': 'MERCHANT'
+        }
+    },
+    'BACK_VILLAGE': {
+        'NAME': "Back Of Village",
+        'DESCRIPTION': "You walk more in to the and see a tavern to the north, a blacksmith to the south,"
+                       " and the castle in the east",
+        'PATHS': {
+            'EAST': "KINGS_CASTLE",
+            'SOUTH': "TAVERN",
+            'NORTH': "BLACKSMITH",
+
+
+        }
     }
-}
