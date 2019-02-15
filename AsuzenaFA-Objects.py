9432fc7de2572af6f81f8fc3f0b994ade6ac9ae0
AsuzenaFA <42383455+AsuzenaFA@users.noreply.github.com>
2019-02-15 08:43:10 -0800

    item

--- AsuzenaFA-Objects.py
+++ AsuzenaFA-Objects.py
@@ -1,13 +1,23 @@
 class Sword(object):
-    def __init__(self, damage=10, speed=5, durability=50, stamina=100, distance=5):
+    def __init__(self, damage=10, speed=5, swing=0, stamina=100):
         self.damage = damage
-        self.range = distance
         self.speed = speed
-        self.durability = durability
         self.grip = True
         self.stamina = stamina
+        self.swing = swing
 
     def shoot(self):
         if self.grip:
-            if self.range <= self.stamina:
-                print()
+            self.swing += 1
+            if self.stamina == 0:
+                print("You have no stamina")
+            elif self.stamina < self.swing:
+                print("You ran out f stamina and swung %s times" % self.swing)
+        else:
+            print("Yuo have no grip")
+
+    def wait(self):
+        self.stamina = 100
+        print("You regained all your stamina")
+
+my_sword(10, )
